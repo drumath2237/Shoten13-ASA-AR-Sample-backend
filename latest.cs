5484385fa19479b0486113c51c779fcbf4415981
drumath <2237k.drumath@gmail.com>
2022-09-10 00:25:50 +0900

    :fire: 書幅に合わせた改行

--- latest.cs
+++ latest.cs
@@ -13,16 +13,17 @@ namespace Shoten13Sample
   {
     [FunctionName("latest")]
     public static async Task<IActionResult> Run(
-        [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)]
-        HttpRequest req,
-        [CosmosDB(
-            databaseName:"Anchors",
-            collectionName:"Items",
-            ConnectionStringSetting ="CosmosDBConnectionString",
-            SqlQuery ="SELECT top 1 c.anchorKey,c.expireOn From c order by c._ts desc"
-        )]
-        IEnumerable<AnchorInfo> anchorInfos,
-        ILogger log)
+      [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)]
+      HttpRequest req,
+      [CosmosDB(
+        databaseName:"Anchors",
+        collectionName:"Items",
+        ConnectionStringSetting ="CosmosDBConnectionString",
+        SqlQuery ="SELECT top 1 c.anchorKey,c.expireOn From c order by c._ts desc"
+      )]
+      IEnumerable<AnchorInfo> anchorInfos,
+      ILogger log
+    )
     {
       var anchorInfo = anchorInfos.FirstOrDefault();
       return new OkObjectResult(anchorInfo);
